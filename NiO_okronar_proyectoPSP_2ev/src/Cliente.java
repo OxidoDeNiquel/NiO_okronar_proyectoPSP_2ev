c93af431e40c42ab009b2fd4ce8b4076399762cc
NiO <119584115+OxidoDeNiquel@users.noreply.github.com>
2024-02-17 18:29:47 +0100

    v17/02_3
    Comunicación de cliente con el servidor enviando mensajes. NC

--- NiO_okronar_proyectoPSP_2ev/src/Cliente.java
+++ NiO_okronar_proyectoPSP_2ev/src/Cliente.java
@@ -1,4 +1,78 @@
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+import javax.swing.*;
+
 public class Cliente {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MarcoCliente mimarco = new MarcoCliente();
+		mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+}
+
+class MarcoCliente extends JFrame {
+
+	public MarcoCliente() {
+		setBounds(600, 300, 280, 350);
+		
+		LaminaMarcoCliente milamina = new LaminaMarcoCliente();
+		add(milamina);
+		setVisible(true);
+	}
+
+}
+
+class LaminaMarcoCliente extends JPanel {
+
+	public LaminaMarcoCliente() {
+		JLabel texto = new JLabel("CLIENTE");
+		add(texto);
+		
+		campo1 = new JTextField(20);
+		add(campo1);
+		
+		miboton = new JButton("Enviar");
+		
+		EnviarTexto mievento = new EnviarTexto();
+		miboton.addActionListener(mievento);
+		
+		add(miboton);
+
+	}
+	
+	private class EnviarTexto implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			try {
+				//Puente para comunicar con el servidor
+				Socket misocket = new Socket("127.0.0.1", 1234);
+				//EL flujo de datos va a circular por el socket (puente)
+				DataOutputStream flujoSalida = new DataOutputStream(misocket.getOutputStream());
+				
+				flujoSalida.writeUTF(campo1.getText());
+				
+				flujoSalida.close();
+				
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				System.out.println(e1.getMessage());
+			}
+		}
+		
+	}
+
+	private JTextField campo1;
+
+	private JButton miboton;
 
 }
